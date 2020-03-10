9e0c9ec6588f180db91b85707a668415dbebfb8d
Christian Bodskov <31687785+Dadango@users.noreply.github.com>
2020-03-10 15:32:18 +0100

    Cranking update
    Now counts frames where you rotate it correctly, allow for a function call or action to be sent to the GameManager (currently a Debug.Log) to start the car/engine. Could have multiple values for (at 200 start noise, 300 start lamps, 400 start shaking, 500 stable start or some shit.

--- Assets/WIP/Bodskov/crankingMotion.cs
+++ Assets/WIP/Bodskov/crankingMotion.cs
@@ -12,29 +12,32 @@ public class crankingMotion : MonoBehaviour
     Vector3 crankOrientation;
     public float speed;
 
+    int framecounter;
+    public int fram_limit;
+
+
     private void Start()
     {
         Cursor.lockState = CursorLockMode.None;
     }
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButton(0)) {
             mouseOrientation = (Input.mousePosition - new Vector3(Screen.width / 2, Screen.height / 2, 20)) - crankAnchor.position;
             crankOrientation = crankHandle.position - crankAnchor.position;
-            //Debug.DrawRay(transform.position, crankOrientation, Color.red);
-            //Debug.DrawRay(transform.position, mouseOrientation, Color.blue);
+
             float angle = Vector3.SignedAngle(mouseOrientation, crankOrientation, Vector3.forward);
             if (!(angle - 10 <= 0 && angle + 10 >= 0)) {
                 int sign = -1;
                 if (angle <= 0) { sign = 1; }
+                framecounter += sign;
+                if (framecounter < 0) { framecounter = 0; }
                 transform.rotation *= Quaternion.AngleAxis((speed * Time.deltaTime)*sign, Vector3.forward);
+                //if (framecounter%10 == 0 ) { Debug.Log(framecounter); }
+                if (framecounter >= fram_limit) { framecounter = 0; Debug.Log("The car started!"); }
             }
         }
     }
 }
-
-//sounds of dying car before breakdown (or just to get you worried for no reason)
-
-//Curser.lockState = CurserLockMode.None;
-//crank center - cran pos = vector (change rotation to match this vector)
