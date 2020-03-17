235a1937c8417265d32f2a751d4211661f138574
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2020-03-17 08:29:29 +0100

    First person move and look

---
+++ Assets/WIP/Stefan/FirstPersonLook.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonLook : MonoBehaviour
+{
+    private bool lockCursor = true;
+
+    public Vector2 mouseSmoothing = new Vector2(2, 2); //TODO: Try to apply smoothing?
+    private Vector2 _smoothMouse;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseLook();
+    }
+
+    private void MouseLook()
+    {
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+
+        transform.Rotate(0, mouseInput.x, 0);
+    }
+}
---
+++ Assets/WIP/Stefan/FirstPersonMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    public float speed = 3.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Motion();
+    }
+
+    private void Motion()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontal, 0, vertical).normalized;
+        movementDirection = transform.TransformDirection(movementDirection);
+
+        float magnitude = Mathf.Clamp01(new Vector2(horizontal, vertical).magnitude);
+
+        float curSpeed = speed * magnitude;
+        _controller.SimpleMove(movementDirection * curSpeed);
+    }
+
+}
