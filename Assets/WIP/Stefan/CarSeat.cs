df5a81350c717d10570ef14828144ad4cf7d1b21
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2020-03-17 12:14:13 +0100

    Work on entering and exiting car

---
+++ Assets/WIP/Stefan/CarSeat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSeat : MonoBehaviour
+{
+    public bool playerInSeat = false;
+    //public CarDoor leftDoor;
+    public CarDoor rightDoor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //leftDoor.seat = this;
+        rightDoor.seat = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/WIP/Stefan/CarSeatManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSeatManager : MonoBehaviour
+{
+    public bool playerInSeat = false;
+    public CarDoor leftDoor;
+    public CarDoor rightDoor;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
