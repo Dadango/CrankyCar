b86d2ec40505633fe00096479fd207bc0154932c
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2020-03-17 11:35:28 +0100

    Camera vertical look works + clamping

--- Assets/WIP/Stefan/FirstPersonLook.cs
+++ Assets/WIP/Stefan/FirstPersonLook.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 public class FirstPersonLook : MonoBehaviour
 {
     private bool lockCursor = true;
+    public bool invertedY = false;
+    public float yAxisRotRange = 90f;
 
-    public Vector2 mouseSmoothing = new Vector2(2, 2); //TODO: Try to apply smoothing?
+    public Camera playerCam;
+    private float rotationY = 0f;
+
+    // public Vector2 mouseSmoothing = new Vector2(2, 2); //TODO: Try to apply smoothing?
+    public float mouseYSensitivity = 1f;
     private Vector2 _smoothMouse;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -30,7 +36,15 @@ public class FirstPersonLook : MonoBehaviour
         }
 
         Vector2 mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        if (playerCam != null)
+        {
+            // playerCam.transform.Rotate(invertedY ? mouseInput.y : -mouseInput.y, 0, 0); //NOTE: Works, but is unclamped
 
+            rotationY += mouseInput.y * mouseYSensitivity;
+            rotationY = Mathf.Clamp(rotationY, -yAxisRotRange, yAxisRotRange);
+
+            playerCam.transform.localEulerAngles = new Vector3(-rotationY, playerCam.transform.localEulerAngles.y, playerCam.transform.localEulerAngles.z);
+        }
 
         transform.Rotate(0, mouseInput.x, 0);
     }
