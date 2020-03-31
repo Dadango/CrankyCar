ac10c6fc0fb0d68820ac5009fb98fc0ff90081af
Seyya <mrfm16@student.aau.dk>
2020-03-31 14:55:23 +0200

    sound stuffs
    added sound files

---
+++ Assets/WIP/Martin/soundPlay.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+// The Audio Source component has an AudioClip option.  The audio
+// played in this example comes from AudioClip and is called audioData.
+
+[RequireComponent(typeof(AudioSource))]
+public class soundPlay : MonoBehaviour
+{
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+
+    void Start()
+    {
+        audioSource1.PlayScheduled(AudioSettings.dspTime);
+        double clipLength = audioSource1.clip.samples / audioSource1.clip.frequency;
+        audioSource2.PlayScheduled(AudioSettings.dspTime + clipLength);
+    }
+}
