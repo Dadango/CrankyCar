6545a6f3433b43b1717631419031bc2f43517bfb
Christian Bodskov <cbodsk17@student.aau.dk>
2020-04-07 17:25:05 +0200

    Minor tweaks to monster events

--- Assets/WIP/Bodskov/EntityScript.cs
+++ Assets/WIP/Bodskov/EntityScript.cs
@@ -2,20 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public static class ifHelp{
+    public static bool isWithin(this int value, int min, int max)
+    {
+        return value >= min && value <= max;
+    }
+
+}
+
 public class EntityScript : MonoBehaviour //move me into Eventhandler and gamemanager later!
 {
     public GameObject player;
     public Transform goal;
     public float rotationSpeed;
-    public float moveSpeed;
-    public float severity;
-    public float difficultyMod;
+    public int moveSpeed;
+    public int severity;
     private int diffStep = 800;
+    private bool severe;
 
 
     private void Start()
     {
-        InvokeRepeating("DiffRamp", 10.0f, 2.0f);
         Invoke("eyes", 1.0f);
     }
 
@@ -24,61 +31,64 @@ public class EntityScript : MonoBehaviour //move me into Eventhandler and gamema
         float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(player.transform.position - transform.position), rotationSpeed * Time.deltaTime);//rotation
         transform.position += transform.forward * moveSpeed * Time.deltaTime;//movement
-        severity = difficultyMod + (500 - distanceToPlayer); //severity level of nme interactions, based off distance and difficulty
+        severity = (int)(500 - distanceToPlayer); //severity level of nme interactions, based off distance and difficulty
         moveSpeed = severity <= 150 ? 8 : 3;
     }
 
-    void DiffRamp() {
-        if (Vector3.Distance(player.transform.position, goal.position)-200 < diffStep) { //the closer you get the more difficulty ramps up
-            diffStep -= 80;
-            difficultyMod += 10; //difficulty escalation as player approaches the goal. Increase or change to multiplication?
-        };
-    }
 
     public List<GameObject> eventPrefabs;
     //private List<GameObject> runningSounds;
 
+
+
+
     void decideAction()
     {
+        severe = false;
         if (severity < 100)
         {
             print("ambience + occasional owls");
         }
-        else if (100 < severity && severity < 150)
+        else if (severity.isWithin(100,150))
         {
             print("ambience + leaves + owls");
         }
-        else if (150 < severity && severity < 200)
+        else if (severity.isWithin(150, 200))
         {
             print("ambience + leaves + owls + footsteps");
         }
-        else if (200 < severity && severity < 250)
+        else if (severity.isWithin(200, 250))
         {
             print("ambience + leaves + owls + footsteps + eyes");
         }
-        else if (250 < severity && severity < 300) {
+        else if (severity.isWithin(250, 300)) {
             print("ambience + leaves + owls + footsteps + eyes + growling");
         }
-        else if (300 < severity && severity < 350)
+        else if (severity.isWithin(300, 350))
         {
             print("leaves + footsteps");
         }
-        else if (350 < severity) {
+        else if (severity.isWithin(350, 400)) {
+            severe = true;
             print("many eyes + possibly shadowy figure");
         }
-
+        //490 u die, 400 -> 490 it becomes faintly visible?
         //foreach (GameObject sound in runningSounds) {
 
         //}
-        GameObject owl = Instantiate(eventPrefabs[0], new Vector3(player.transform.position.x-10, player.transform.position.y, player.transform.position.z+10), new Quaternion(0, 0, 0, 0));
+        //GameObject owl = Instantiate(eventPrefabs[0], new Vector3(player.transform.position.x-10, player.transform.position.y, player.transform.position.z+10), new Quaternion(0, 0, 0, 0));
         //wait for length of audio clip then
-        GameObject.Destroy(owl);
+        //GameObject.Destroy(owl);
     }
+    
 
     void eyes() {
         int lifeSpan = Random.Range(10, 31);
+        int distance = severe ? 1 : moveSpeed;
+        int offset = Random.Range(-10, 10);
+        int offsetY = Random.Range(0, 10);
         Vector3 direction = player.transform.position - transform.position;
-        Vector3 position = new Vector3(player.transform.position.x - direction.x/3, player.transform.position.y, player.transform.position.z - direction.z/3);
+        Vector3 position = new Vector3((player.transform.position.x - direction.x/distance) + offset, player.transform.position.y + offsetY, (player.transform.position.z - direction.z/distance)+offset);
         GameObject eyes = Instantiate(eventPrefabs[3], position, Quaternion.LookRotation(player.transform.position - position));
         GameObject.Destroy(eyes, lifeSpan);
     }
