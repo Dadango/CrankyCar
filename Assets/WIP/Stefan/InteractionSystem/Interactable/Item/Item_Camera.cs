cbf43b2aca4186bd48dd78dafaa32f407c630fdc
Stefan Nordborg Eriksen <the-grey-side@hotmail.com>
2020-04-13 14:53:05 +0200

    Most of Item_Camera done. Needs further integration with Entity

--- Assets/WIP/Stefan/InteractionSystem/Interactable/Item/Item_Camera.cs
+++ Assets/WIP/Stefan/InteractionSystem/Interactable/Item/Item_Camera.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Item_Camera : Item
 {
     [Header("Camera parameters")]
+    public Light flashLight;
     [Tooltip("Determines how much the player should be facing towards the enemy to hit. -1 is exact opposite direction, +1 is exactly towards.")]
     [Range(-1f, 1f)]
     public float facingHitThreshold = 0.5f;
@@ -12,9 +13,13 @@ public class Item_Camera : Item
     public float distanceHitThreshold = 50f;
     [Tooltip("Controls the light intensity animation from the camera's flashbulb.")]
     public AnimationCurve flashFallof = new AnimationCurve();
-    public float flashDuration = 1f;
+    public float flashDuration = 2f;
+    private float _maxIntensity;
     public int startingCharges = 5;
     private int _charges;
+    public float cooldown = 2;
+    private float _cooldownTimer;
+
     public Transform enemyTransform; //TODO: Kinda placeholder for now. Integrate with EntityScript!
     private Vector3 enemyLocation
     { get { return enemyTransform.position; } }
@@ -24,12 +29,15 @@ public class Item_Camera : Item
     {
         base.Start(); // runs the code from the base
         _charges = startingCharges;
+        _maxIntensity = flashLight.intensity;
+        _cooldownTimer = cooldown;
+        Flash();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        CooldownTimer();
     }
 
 
@@ -43,29 +51,55 @@ public class Item_Camera : Item
         throw new System.NotImplementedException(); //TODO: Maybe prompt about remaining charges?
     }
 
+    /// <summary>
+    /// Cooldown timer for the Camera. 
+    /// </summary>
+    private void CooldownTimer()
+    {
+        if (_cooldownTimer < cooldown)
+        {
+            _cooldownTimer += Time.deltaTime;
+            Flash();
+        }
+    }
+
+    /// <summary>
+    /// Sets the intensity of the flash, based on cooldownTimer.
+    /// </summary>
+    private void Flash()
+    {
+        flashLight.intensity = flashFallof.Evaluate(_cooldownTimer / flashDuration)*_maxIntensity;
+    }
+
     private void ShootCamera()
     {
-        if (_charges > 0)
+        if (_charges > 0) //If there are charges left
         {
-            _charges -= 1;
+            if (_cooldownTimer >= cooldown) //If the cooldown has passed
+            {
+                _charges -= 1;
+                _cooldownTimer = 0f;
 
-            Player player = GameManager.Instance.Player;
+                Player player = GameManager.Instance.Player;
 
-            //Find vector and distance to enemy
-            Vector3 vectorToEnemy = enemyLocation - player.transform.position;
-            float distanceToEnemy = vectorToEnemy.magnitude;
+                //Find vector and distance to enemy
+                Vector3 vectorToEnemy = enemyLocation - player.transform.position;
+                float distanceToEnemy = vectorToEnemy.magnitude;
 
-            //Find player orientation
-            Vector3 playerViewDirection = player.firstPersonLook.playerCam.transform.forward;
+                //Find player orientation
+                Vector3 playerViewDirection = player.firstPersonLook.playerCam.transform.forward;
 
-            //Dot product the two (normalized) directions to get how much the player is looking towards the enemy
-            float facingAmount = Vector3.Dot(playerViewDirection, (vectorToEnemy / distanceToEnemy));
+                //Dot product the two (normalized) directions to get how much the player is looking towards the enemy
+                float facingAmount = Vector3.Dot(playerViewDirection, (vectorToEnemy / distanceToEnemy));
 
-            Debug.Log($"Facing: {facingAmount}, Distance: {distanceToEnemy}, FacingTreshold: {facingAmount >= facingHitThreshold}, DistanceThreshold: {distanceToEnemy <= distanceHitThreshold}");
+                Debug.Log($"Facing: {facingAmount}, Distance: {distanceToEnemy}, FacingTreshold: {facingAmount >= facingHitThreshold}, DistanceThreshold: {distanceToEnemy <= distanceHitThreshold}");
 
+                if(facingAmount >= facingHitThreshold && distanceToEnemy <= distanceHitThreshold)
+                {
+                    //TODO: Do something if player is looking enough towards enemy (+ close enough?).
 
-            //Do something if player is looking enough towards enemy (+ close enough?).
-            //TODO: some sort of flash. Intensity curve?
+                }
+            }
         }
         else
         {
