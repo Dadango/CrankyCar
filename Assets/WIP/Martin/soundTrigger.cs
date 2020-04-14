b242410e072b28754b32475bed85828ab8f7fb34
Seyya <mrfm16@student.aau.dk>
2020-04-14 10:23:20 +0200

    sound changes and move in forestterrain

---
+++ Assets/WIP/Martin/soundTrigger.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class soundTrigger : MonoBehaviour
+{
+
+    public AudioSource audio;
+    public GameObject player;
+    void Update()
+    {
+
+    }
+    void OnTriggerStay(Collider other)
+    {
+        audio.Play();
+    }
+
+}
