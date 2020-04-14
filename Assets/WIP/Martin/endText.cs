0e311e0d4aa4a3ad3511132e844836d440b33908
Seyya <mrfm16@student.aau.dk>
2020-04-14 17:45:48 +0200

    changed car sounds volume and awake on start, make endText trigger

---
+++ Assets/WIP/Martin/endText.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject textEnd;
+
+    void OnTriggerEnter(Collider other)
+    {
+        textEnd.gameObject.GetComponent<MeshRenderer>().enabled = true;
+    }
+}
